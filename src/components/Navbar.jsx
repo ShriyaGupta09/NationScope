249377eac65700af00f33983a44bcfbbd4ae5e37
ShriyaGupta09 <9shriyag@gmail.com>
2024-11-23 00:32:17 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,69 @@
-const Navbar = ({setCategory}) => {
+const Navbar = ({ setCategory }) => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" style={{border: "0.4px solid black"}}>
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img src="/src/assets/newspaper.png" width="35" height="35" alt="logo"/>
-                </a>
-                <span className="navbar-brand mb-0 h1">NationScope</span>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("technology")} href="#">Technology</div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("Business")} href="#">Business</div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("Science")} href="#">Science</div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("Health")} href="#">Health</div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("Sports")} href="#">Sports</div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("Entertainment")} href="#">Entertainment</div>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link" onClick={() => setCategory("Politics")} href="#">Politics</div>
-                        </li>
-                    </ul>
+      <nav
+        className="navbar navbar-expand-lg bg-body-tertiary"
+        data-bs-theme="dark"
+        style={{ border: "0.4px solid black" }}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="/src/assets/newspaper.png" width="35" height="35" alt="logo" />
+          </a>
+          <span className="navbar-brand mb-0 h1">NationScope</span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("technology")}>
+                  Technology
                 </div>
-            </div>
-        </nav>
-    )
-}
-
-export default Navbar;
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("Business")}>
+                  Business
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("Science")}>
+                  Science
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("Health")}>
+                  Health
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("Sports")}>
+                  Sports
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("Entertainment")}>
+                  Entertainment
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="nav-link" onClick={() => setCategory("Politics")}>
+                  Politics
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
